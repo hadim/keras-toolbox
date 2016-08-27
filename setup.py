65833ec202aca20ff7117c2b100d632333e91b1e
Hadrien Mary <hadrien.mary@gmail.com>
2016-08-27 15:38:34 -0400

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+from setuptools import find_packages
+
+
+setup(name='keras-toolbox',
+      version='0.1.0',
+      description='Everyday toolbox for Keras',
+      author='Hadrien Mary',
+      author_email='hadrien.mary@gmail.com',
+      url='https://github.com/hadim/keras-toolbox',
+      download_url='https://github.com/hadim/keras-toolbox/archive/master.zip',
+      license='MIT',
+      packages=find_packages())
