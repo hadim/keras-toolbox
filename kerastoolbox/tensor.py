fc17d7aacc6154451f7a7f44e24e718d7f2432aa
Hadrien Mary <hadrien.mary@gmail.com>
2016-08-29 21:35:01 -0400

    Add mask_to_label_images functions

--- kerastoolbox/tensor.py
+++ kerastoolbox/tensor.py
@@ -1,27 +1,46 @@
 import numpy as np
 
 
-def mask_to_category(mask):
+def mask_to_label_images(data, labels_axis=1):
+    """Take a 4D array (n_images, 1, width, height) and use the single dimension axis to transform into
+    a multiple dimensions axis that contain 0 or 1 according to the presence of a label. The labels are
+    guessed using np.unique(data).
     """
+
+    # Get all the labels in the dataset
+    labels = np.unique(data)
+
+    new_dims = list(data.shape)
+    new_dims[labels_axis] = len(labels)
+
+    data_labels = np.zeros(new_dims)
+    data_labels = data_labels.swapaxes(0, labels_axis)
+
+    for i, label in enumerate(labels):
+        mask = (data == label).squeeze(axis=labels_axis)
+        data_labels[i][mask] = 1
+
+    data_labels = data_labels.swapaxes(0, labels_axis)
+
+    return data_labels
+
+
+def label_images_to_mask(data, labels_axis=1):
+    """Revert mask_to_label_images()
     """
-    
-    assert (len(mask.shape) == 4)
-    assert (mask.shape[1] == 1)
-    
-    im_h = mask.shape[2]
-    im_w = mask.shape[3]
-    
-    mask = np.reshape(mask,(mask.shape[0], im_h * im_w))
-    new_mask = np.empty((mask.shape[0], im_h * im_w, 2))
-    
-    for i in range(mask.shape[0]):
-        for j in range(im_h * im_w):
-            
-            if  mask[i,j] == 0:
-                new_mask[i,j,0] = 1
-                new_mask[i,j,1] = 0
-            else:
-                new_mask[i,j,0] = 0
-                new_mask[i,j,1] = 1
-                
-    return new_mask
+
+    new_dims = list(data.shape)
+    new_dims[labels_axis] = 1
+
+    data_mask = np.zeros(new_dims)
+    data_mask = data_mask.swapaxes(0, labels_axis)
+
+    data = data.swapaxes(0, labels_axis)
+
+    for i in range(data.shape[0]):
+        data_mask[0][data[i] == 1] = i
+
+    data = data.swapaxes(0, labels_axis)
+    data_mask = data_mask.swapaxes(0, labels_axis)
+
+    return data_mask
