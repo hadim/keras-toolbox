e76d16851578eb437771cff7fe0345cfbc473abf
Hadrien Mary <hadrien.mary@gmail.com>
2016-08-30 21:45:26 -0400

    Release 0.1.1

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages
 
 
 setup(name='keras-toolbox',
-      version='0.1.0',
+      version='0.1.1',
       description='Everyday toolbox for Keras',
       author='Hadrien Mary',
       author_email='hadrien.mary@gmail.com',
